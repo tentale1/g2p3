cfbb3b2283371e44fb292c2ed8ae2f4f052f29bc
tentale1 <jalvarez2153@universidadean.edu.co>
2019-08-19 17:10:39 -0500

    Actualizacion de autores

--- colectivo.java
+++ colectivo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * Programa para hallar viajes, ganancia y costo
  * 
- * @author Carlos Alvarez 
+ * @author Sandra Martin, Israel Severiche, Jose Alvarez
  * @version 20190817
  */
 public class colectivo
